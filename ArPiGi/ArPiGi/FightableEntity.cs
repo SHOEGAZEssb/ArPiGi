a80d70156ce920cb5c6c9754985a42bf858710a5
coczero <timstadler905@googlemail.com>
2015-01-27 20:28:00 +0100

    Added player class,  added level and speed property to FightableEntity,

--- ArPiGi/ArPiGi/FightableEntity.cs
+++ ArPiGi/ArPiGi/FightableEntity.cs
@@ -1,24 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 
 namespace ArPiGi
 {
+  /// <summary>
+  /// Represents an entity that can fight and can be fought.
+  /// </summary>
   abstract class FightableEntity : EntityBase
   {
+    private int _level;
     private int _hp;
     private int _mp;
     private int _strength;
     private int _defense;
+    private int _speed;
     private Skill[] _skills;
+
+    public int Level
+    {
+      get { return _level; }
+      private set { _level = value; }
+    }
 
     public int HP
     {
       get { return _hp; }
-      private set { _mp = value; }
+      private set { _hp = value; }
     }
 
     public int MP
@@ -39,19 +45,27 @@ namespace ArPiGi
       private set { _defense = value; }
     }
 
+    public int Speed
+    {
+      get { return _speed; }
+      private set { _speed = value; }
+    }
+
     public Skill[] Skills
     {
       get { return _skills; }
       private set { _skills = value; }
     }
 
-    public FightableEntity(int hp, int mp, int strength, int defense, Skill[] skills, string name, Image[] sprites)
+    public FightableEntity(int level, int hp, int mp, int strength, int defense, int speed, Skill[] skills, string name, Image[] sprites)
       : base(name, sprites)
     {
+      Level = level;
       HP = hp;
       MP = mp;
       Strength = strength;
       Defense = defense;
+      Speed = speed;
       Skills = skills;
     }
   }
---
+++ ArPiGi/ArPiGi/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+namespace ArPiGi
+{
+  class Player : FightableEntity
+  {
+    private int _xp;
+    private Point _location;
+
+    public int XP
+    {
+      get { return _xp; }
+      private set { _xp = value; }
+    }
+
+    public Point Location
+    {
+      get { return _location; }
+      private set { _location = value; }
+    }
+
+    public Player(int level, int hp, int mp, int strength, int defense, int speed, Skill[] skills, string name, Image[] sprites)
+      : base(level, hp, mp, strength, defense, speed, skills, name, sprites)
+    {
+      XP = 0;
+    }
+  }
+}
